02ea3dbf5b56b3a0e9788fa1291b6e0c89c5d7df
sharonandisi <sharonandisi.28@gmail.com>
2018-11-19 00:17:47 +0300

    we create configuration classes

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,6 @@ from app import app
 def index():
     title = 'Welcome to Pitches&Creme'
     return render_template('index.html',title=title, pitches=pitches)
-    
+if __name__ == '__main__':
+    app.run(debug = True)
+
