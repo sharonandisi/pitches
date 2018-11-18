f28c325167eccba241128c42685d5b511b7c3e9a
sharonandisi <sharonandisi.28@gmail.com>
2018-11-19 00:22:04 +0300

    we create our index.html template file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from config import DevConfig
 
 #initializing application
 
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
 
 from app import views
