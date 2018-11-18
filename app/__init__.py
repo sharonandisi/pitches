d3b2e8c4c7a74804c0d71b5aa937a9919b0313cd
sharonandisi <sharonandisi.28@gmail.com>
2018-11-19 01:33:28 +0300

    we register the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,22 @@ from config import DevConfig
 from flask_bootstrap import Bootstrap
 
 #initializing application
-app = Flask(__name__, instance_relative_config = True)
+def create_app(config_name):
+    app = Flask(__name__)
 
 # Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+app.config.from_object(config_options[config_name])
+config_options[config_name].init_app(app)
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
+db.init_app(app)
+login_manager.init_app(app)
+mail.init_app(app)
+
+# Registering the blueprint
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
 
 from app import views
--- manage.py
+++ manage.py
@@ -1,2 +1,8 @@
 from app import create_app,db 
-from app.models import User 
+from app.models import User, Comments, Pitch
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Comments = Comments, Pitch = Pitch)
+if __name__ == '__main__':
+    manager.run()
