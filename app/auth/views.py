429f7f5a7b0519a64eb59cc9af62e67dad4fe10f
sharonandisi <sharonandisi.28@gmail.com>
2018-11-20 10:44:15 +0300

    we render the form in our login.html using the wtf.quick_forms()

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,25 @@
 from flask import render_template,redirect,url_for
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
 from . import auth
 from .. import db
 
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
+        flash('Invalid username or Password')
+
+    title = "watchlist login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+    
 auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
