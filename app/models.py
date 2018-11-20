517a1d8eb726f5071b024adc06458a953d8df523
sharonandisi <sharonandisi.28@gmail.com>
2018-11-20 10:25:08 +0300

    we add our custom validators

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from .import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(UserMixin, db.Model):
   __tablename__='users'
