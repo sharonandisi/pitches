4688412ce4f8b66f0445406b98a56928140475dc
sharonandisi <sharonandisi.28@gmail.com>
2018-11-20 11:16:56 +0300

    we setup the SMTP server and configure the port to use the gmail SMTP server port

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ import os
 class Config:
    SECRET_KEY = os.environ.get('SECRET_KEY')
    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sharon:sharon@localhost/projectpitches'
-
+   #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
@@ -29,5 +34,4 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-# 'test':TestConfig
 }
