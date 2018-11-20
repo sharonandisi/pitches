e21873c1291fb76ba869761cca3e3e6adda3e25d
sharonandisi <sharonandisi.28@gmail.com>
2018-11-20 11:30:09 +0300

    we call the function inside our register view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,6 +28,8 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message('Welcome to pitches and creme','email/welcome_user',user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
