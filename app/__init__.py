c59df36e9f694ac13526e30d975952d82853fd3e
sharonandisi <sharonandisi.28@gmail.com>
2018-11-20 11:22:58 +0300

    we initialize the extension

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 db = SQLAlchemy()
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
 
 #initializing application
 def create_app(config_name):
